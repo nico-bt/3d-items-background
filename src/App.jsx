110c3283f8ae8f1b0ffbbd90a3fecdd7f4135230
Nico-Battaglia <nico-battaglia@hotmail.com>
2023-07-09 13:59:13 -0300

    First commit, basic structures and npm packages

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <>
+      <section>
+        <h1>Section</h1>
+      </section>
+      <section>
+        <h1>Section</h1>
+      </section>
+      <section>
+        <h1>Section</h1>
+      </section>
+    </>
+  )
+}
+
+export default App
